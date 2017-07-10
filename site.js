a28e20ed8620295352a2088818b6c5463ae1f868
pornister <pornister@users.noreply.github.com>
2017-07-10 14:42:21 +0530

    shuffle js

--- site.js
+++ site.js
@@ -1,12 +1,10 @@
-<script type="text/javascript">
-	$(document).ready(function(){
-		$('.shuffle').shuffle();
-	});
-	$.fn.shuffle=function () {
-    var parent = $(".shuffle");
-    var divs = parent.children();
-    while (divs.length) {
-        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-    };
+$(document).ready(function(){
+	$('.shuffle').shuffle();
+});
+$.fn.shuffle=function () {
+   var parent = $(".shuffle");
+   var divs = parent.children();
+   while (divs.length) {
+       parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+   };
 };
-</script>
